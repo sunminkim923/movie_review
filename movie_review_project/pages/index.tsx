9e79ae7841e6302ff28cd2be6889b469daeb5c79
sunmin <sunmin.dev94@gmail.com>
2022-10-19 18:54:33 +0900

    네이버 api 연동 중

--- movie_review_project/pages/index.tsx
+++ movie_review_project/pages/index.tsx
@@ -1,54 +1,61 @@
 import axios from "axios";
 import dayjs from "dayjs";
-import { useEffect, useState } from "react";
-import { Radio } from "antd";
+import { ChangeEvent, useEffect, useState } from "react";
+import { Button, Input, Radio } from "antd";
 
 interface BoxOfficeList {
   nation: string;
 }
 
 export default function Home() {
+  const clientId = "HfFdJg8CoER4nIqrd6pL";
+  const clientSecret = "TsmSf6MrKl";
+
   const apiKey = "b001a74fb346c75e6f384f6e8ca1a3a7";
   const [boxOfficeList, setBoxOfficeList] = useState([]);
 
+  const [name, setName] = useState("");
+
   useEffect(() => {
-    getBoxOfficeList(null);
+    getMovieList();
   }, []);
 
-  async function getBoxOfficeList(nation: string | null) {
+  async function getMovieList() {
     try {
       const result = await axios({
-        method: "GET",
-        url: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json",
+        method: "get",
+        url: "https://openapi.naver.com/v1/search/movie.json",
+        headers: {
+          "X-Naver-Client-Id": clientId,
+          "X-Naver-Client-Secret": clientSecret,
+          Host: "openapi.naver.com",
+          Accept: "*/*",
+          "User-Agent": "curl/7.49.1",
+          "Access-Control-Allow-Origin": "*",
+        },
         params: {
-          key: apiKey,
-          targetDt: dayjs(new Date()).format("20221018"),
-          repNationCd: nation,
+          query: decodeURIComponent("괴물"),
         },
       });
 
-      setBoxOfficeList(result.data.boxOfficeResult.dailyBoxOfficeList);
+      console.log(result);
     } catch (error) {
       console.log(error, "error");
     }
   }
 
-  console.log(boxOfficeList);
-
-  function onChangeNation(event: string | null) {
-    getBoxOfficeList(event.target.value);
+  function onChangeName(event: ChangeEvent) {
+    setName(decodeURIComponent(event.target.value));
   }
-
   return (
     <div>
-      <Radio.Group defaultValue={null} onChange={onChangeNation}>
-        <Radio value={null}>전체</Radio>
-        <Radio value="K">국내</Radio>
-        <Radio value="F">해외</Radio>
-      </Radio.Group>
-      {boxOfficeList?.map((data) => (
-        <div>{data?.movieNm}</div>
-      ))}
+      {/*<Radio.Group defaultValue={null} onChange={onChangeNation}>*/}
+      {/*  <Radio value={null}>전체</Radio>*/}
+      {/*  <Radio value="K">국내</Radio>*/}
+      {/*  <Radio value="F">해외</Radio>*/}
+      {/*</Radio.Group>*/}
+      <Input onChange={onChangeName} />
+      <Button onClick={() => getMovieList()}>검색</Button>
     </div>
   );
 }
