faaa1e3b81fa54705f0efff78bf02aa5e074d25f
sunmin <sunmin.dev94@gmail.com>
2022-10-19 13:13:18 +0900

    api 테스트 연결

--- movie_review_project/pages/_app.tsx
+++ movie_review_project/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "../src/layout/header";
+import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
