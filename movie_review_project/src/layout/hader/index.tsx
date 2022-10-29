d31be0058f577876c283e8aed94f8dd11067d765
sunmin <sunmin.dev94@gmail.com>
2022-10-29 18:54:53 +0900

    이전 프로젝트 제거

--- movie_review_project/src/layout/hader/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <div className="flex items-center h-[60px] px-[16px] bg-gradient-to-r from-slate-600 to-slate-500">
-      <div className="text-lg font-medium text-white">영화가 좋다.</div>
-    </div>
-  );
-}
--- movie_review_project/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.{html,js,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
