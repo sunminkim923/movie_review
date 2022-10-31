404013ca44716122449a4f6527b3f8ccfa862e5e
sunmin <sunmin.dev94@gmail.com>
2022-10-31 23:53:56 +0900

    react slick 적용

--- movie_review/pages/index.tsx
+++ movie_review/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Carousel } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Slider from "react-slick";
 
 export default function Home() {
   const posterBaseUrl = "https://image.tmdb.org/t/p/original";
@@ -26,22 +27,42 @@ export default function Home() {
     }
   }
 
+  const settings = {
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    vertical: false,
+    variableWidth: true,
+  };
+
   return (
-    <>
-      <div className="flex">
-        {popularList?.map((data) => (
-          <div className=" w-[200px] h-[200px]">
-            <img
-              src={posterBaseUrl + data.poster_path}
-              style={{ width: "100%", height: "100%" }}
-            />
-          </div>
-        ))}
+    <div>
+      <div className="w-[100%] h-[500px] bg-black ">
+        <Slider {...settings}>
+          {popularList?.map((data) => (
+            <div className="w-[100%] h-[500px]">
+              <img
+                className="h-[100%]"
+                src={posterBaseUrl + data.poster_path}
+              />
+            </div>
+          ))}
+        </Slider>
       </div>
+
+      {/* <div className="w-[200px] h-[300px]">
+        <img
+          className="h-[100%]"
+          src={posterBaseUrl + popularList[7]?.poster_path}
+        />
+      </div> */}
 
       <div className="bg-red-400 flex justify-center min-h-screen">
         <div className=" w-[70%] h-[300px] bg-neutral-700">테스트</div>
       </div>
-    </>
+    </div>
   );
 }
