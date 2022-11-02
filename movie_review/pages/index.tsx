6d3473db80b27a84e4a47e310a0f099ebdba4b2b
sunmin <sunmin.dev94@gmail.com>
2022-11-02 10:20:46 +0900

    타이틀 작업중

--- movie_review/pages/index.tsx
+++ movie_review/pages/index.tsx
@@ -36,32 +36,34 @@ export default function Home() {
     slidesToScroll: 1,
     vertical: false,
     variableWidth: true,
+    arrows: false,
+    pauseOnHover: false,
   };
 
   return (
     <div>
-      <div className="w-[100%] h-[500px] bg-black ">
-        <Slider {...settings}>
-          {popularList?.map((data) => (
-            <div className="w-[100%] h-[500px]">
-              <img
-                className="h-[100%]"
-                src={posterBaseUrl + data.poster_path}
-              />
-            </div>
-          ))}
-        </Slider>
+      <div className="flex justify-center bg-black h-[65px] items-center">
+        <div className="w-[95%]  text-white text-[30px]">인기영화</div>
+      </div>
+      <div className="w-[100%] flex justify-center bg-black pb-[35px]">
+        <div className="w-[95%]">
+          <div className="w-[100%] h-[500px] bg-zinc-900">
+            <Slider {...settings}>
+              {popularList?.map((data) => (
+                <div className="w-[100%] h-[500px] py-[4px] px-[2px] ">
+                  <img
+                    className="h-[100%]"
+                    src={posterBaseUrl + data.poster_path}
+                  />
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </div>
       </div>
 
-      {/* <div className="w-[200px] h-[300px]">
-        <img
-          className="h-[100%]"
-          src={posterBaseUrl + popularList[7]?.poster_path}
-        />
-      </div> */}
-
       <div className="bg-red-400 flex justify-center min-h-screen">
-        <div className=" w-[70%] h-[300px] bg-neutral-700">테스트</div>
+        <div className=" w-[80%] h-[300px] bg-neutral-700">테스트</div>
       </div>
     </div>
   );
